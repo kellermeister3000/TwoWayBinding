d0bddb53722b852318970bb0c8c39507c24f6376
Philip Keller <philipjoelkeller@gmail.com>
2023-03-14 14:30:30 -0700

    Initial Commit

---
+++ TwoWayBinding/TwoWayBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoWayBindingApp.swift
+//  TwoWayBinding
+//
+//  Created by Philip Keller on 3/14/23.
+//
+
+import SwiftUI
+
+@main
+struct TwoWayBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
